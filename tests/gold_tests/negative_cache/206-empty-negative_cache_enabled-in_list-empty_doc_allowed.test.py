f59e39b5b1c460e0ef129e46dbae4401d065db93
Hiroaki Nakamura <hnakamur@gmail.com>
2019-04-26 06:06:22 +0000

    Add tests for 206 negative cache

---
+++ tests/gold_tests/negative_cache/206-empty-negative_cache_enabled-in_list-empty_doc_allowed.test.py
@@ -0,0 +1,83 @@
+'''
+Test negative cache 206 empty response with negative_caching_enabled=1, in list and allow_empty_doc=1
+'''
+#  Licensed to the Apache Software Partial Contentation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+Test.Summary = '''
+Test negative cache 206 empty response with negative_caching_enabled=1, in list and allow_empty_doc=1
+'''
+
+# Needs Curl
+Test.SkipUnless(
+    Condition.HasProgram("curl", "curl needs to be installed on system for this test to work"),
+)
+Test.ContinueOnFail = True
+
+# Define default ATS
+ts = Test.MakeATSProcess("ts")
+server = Test.MakeOriginServer("server")
+
+#**testname is required**
+testName = ""
+request_header1 = {"headers": "GET /206 HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
+response_header1 = {"headers": "HTTP/1.1 206 Partial Content\r\nContent-Length: 0\r\nConnection: close\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
+server.addResponse("sessionlog.json", request_header1, response_header1)
+
+# ATS Configuration
+ts.Disk.plugin_config.AddLine('xdebug.so')
+ts.Disk.records_config.update({
+    'proxy.config.cache.ram_cache.algorithm': 1,
+    'proxy.config.cache.ram_cache.use_seen_filter': 1,
+    'proxy.config.diags.debug.enabled': 1,
+    'proxy.config.diags.debug.tags': 'http_trans',
+    'proxy.config.diags.output.debug': 'L',
+    'proxy.config.http.cache.allow_empty_doc': 1,
+    'proxy.config.http.cache.http': 1,
+    'proxy.config.http.negative_caching_enabled': 1,
+    'proxy.config.http.negative_caching_list': '206',
+    'proxy.config.http.response_via_str': 'ApacheTrafficServer',
+    'proxy.config.http.wait_for_cache': 1,
+    'proxy.config.http.insert_age_in_response': 0,
+})
+
+ts.Disk.remap_config.AddLine(
+    'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
+)
+
+# Test 1 - 206 empty response and cache miss
+tr = Test.AddTestRun()
+tr.Processes.Default.StartBefore(server)
+tr.Processes.Default.StartBefore(Test.Processes.ts, ready=1)
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/206'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/empty-cache_no_fill-content_length_0.gold"
+tr.StillRunningAfter = ts
+
+# Test 2 - 206 empty response and cache miss
+tr = Test.AddTestRun()
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/206'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/empty-cache_no_fill-content_length_0.gold"
+tr.StillRunningAfter = ts
+
+# Test 3 - 206 empty response and cache miss
+tr = Test.AddTestRun()
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/206'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/empty-cache_no_fill-content_length_0.gold"
+tr.StillRunningAfter = ts
