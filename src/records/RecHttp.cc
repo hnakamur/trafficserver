e9dc0c65611417b731670400666a0f8302e72f3a
Hiroaki Nakamura <hnakamur@gmail.com>
2025-03-23 17:31:14 +0900

    Return std::pair<std::string, RecErrT> from RecGetRecordString_Xmalloc
    and rename it to RecGetRecordStringAlloc.
    
    Also change set_paths_helper to take std::string_view for the first
    and the second arguments.

--- src/records/RecHttp.cc
+++ src/records/RecHttp.cc
@@ -282,14 +282,10 @@ HttpProxyPort::checkPrefix(const char *src, char const *prefix, size_t prefix_le
 bool
 HttpProxyPort::loadConfig(std::vector<self> &entries)
 {
-  char *text;
-  bool  found_p;
-
-  found_p = RecGetRecordString_Xmalloc(PORTS_CONFIG_NAME, &text) == REC_ERR_OKAY;
-  if (found_p) {
-    self::loadValue(entries, text);
+  auto [text, err]{RecGetRecordStringAlloc(PORTS_CONFIG_NAME)};
+  if (err == REC_ERR_OKAY) {
+    self::loadValue(entries, text.c_str());
   }
-  ats_free(text);
 
   return 0 < entries.size();
 }
@@ -742,11 +738,10 @@ ts_host_res_global_init()
 {
   // Global configuration values.
   host_res_default_preference_order = HOST_RES_DEFAULT_PREFERENCE_ORDER;
-  auto ip_resolve{const_cast<char *>(RecGetRecordString_Xmalloc("proxy.config.hostdb.ip_resolve").first.data())};
-  if (ip_resolve) {
-    parse_host_res_preference(ip_resolve, host_res_default_preference_order);
+  auto ip_resolve{RecGetRecordStringAlloc("proxy.config.hostdb.ip_resolve").first};
+  if (!ip_resolve.empty()) {
+    parse_host_res_preference(ip_resolve.c_str(), host_res_default_preference_order);
   }
-  ats_free(ip_resolve);
 }
 
 // Whatever executable uses librecords must call this.
