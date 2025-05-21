4b06b3e93f6c7850e0fe007799c936377030dbcf
Hiroaki Nakamura <hnakamur@gmail.com>
2025-05-21 22:21:43 +0900

    Change dir_set_next to inline function

--- src/iocore/cache/P_CacheDir.h
+++ src/iocore/cache/P_CacheDir.h
@@ -282,13 +282,19 @@ dir_set_pinned(Dir *e, int v)
 }
 
 // Bit 2:15 is unused.
+
 static inline uint32_t
 dir_next(const Dir *e)
 {
   return e->w[3];
 }
 
-#define dir_set_next(_e, _o) (_e)->w[3] = (uint16_t)(_o)
+static inline void
+dir_set_next(Dir *e, int o)
+{
+  e->w[3] = static_cast<uint16_t>(o);
+}
+
 #define dir_prev(_e)         (_e)->w[2]
 #define dir_set_prev(_e, _o) (_e)->w[2] = (uint16_t)(_o)
 
